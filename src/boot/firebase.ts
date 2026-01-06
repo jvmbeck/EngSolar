45b1c24c297754e03ed9b5bd76e20c237c55196a
jvmbeck <jmadridbeck@gmail.com>
2026-01-06 19:10:53 -0300

    added onSnapshot listener to update projects in real-time

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -2,11 +2,18 @@ import { boot } from 'quasar/wrappers';
 import { auth } from 'src/key/configKey';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useUserStore } from 'src/stores/user-store';
+import { useProjectStore } from 'src/stores/project-store';
 
 export default boot(() => {
   const userStore = useUserStore();
 
   onAuthStateChanged(auth, (user) => {
     void userStore.setAuthUser(user);
+    const projectStore = useProjectStore();
+    if (user) {
+      projectStore.startProjectsListener();
+    } else {
+      projectStore.stopProjectsListener();
+    }
   });
 });
